b0e7515fb21fce87e25ed71385b59b201b42231f
Leeminsu67 <alstn67@gmail.com>
2024-05-26 23:36:13 +0900

    openweather API use

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+
+  const weatherData = async () => {
+    const data = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=Busan&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`
+    );
+    setWeather(data);
+  };
+
+  useEffect(() => {
+    // 날씨 데이터 조회
+    weatherData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>{weather ? <div>들고옴</div> : <div>못 들고옴</div>}</Container>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
 
 export default App;
